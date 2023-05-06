4f65f66e8c54884d99e5b4f201a2bf21927269eb
Giovanni Flores <66764596+Giovanni-Flores@users.noreply.github.com>
2023-05-06 16:27:08 -0300

    Add files via upload

---
+++ Aula4/Aula4/Motorcycle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula4
+{
+    internal class Motorcycle
+    {
+        public String name { get; set; }
+        public String brand { get; set; }
+        public int power { get; set; }
+        public int weight { get; set; }
+    }
+}
---
+++ Aula4/Aula4/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula4
+{
+    class Person
+    {
+        public String name { get; set; }
+        public int age { get; set; }
+        public String adress { get; set; }
+
+    }
+}
