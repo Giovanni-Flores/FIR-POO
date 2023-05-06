d0e26f921bd291ecae7032cf8d86596e1df0c12c
Giovanni Flores <66764596+Giovanni-Flores@users.noreply.github.com>
2023-05-06 16:25:08 -0300

    Add files via upload

---
+++ Aula4/Aula4/Aula4/Telephone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula4
+{
+    class Telephone
+    {
+        public String numberTelephone { get; set; }
+        public String mobileOperator { get; set; }
+
+        public String viewNumber()
+        {
+            return numberTelephone;
+        }
+
+        public String viewMobileOperator()
+        {
+            return mobileOperator;
+        }
+
+
+
+    }
+}
