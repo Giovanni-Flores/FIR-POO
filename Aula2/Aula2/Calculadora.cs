dede33d3ee9251a99caf89aeb1f4054869953bd4
Giovanni Flores <66764596+Giovanni-Flores@users.noreply.github.com>
2023-05-06 15:19:20 -0300

    Add files via upload

--- Aula2/Aula2/Calculadora.cs
+++ Aula2/Aula2/Calculadora.cs
@@ -9,14 +9,17 @@ namespace Aula2
     class Calculadora
     {
         public int valor { get; set; }
+        public int a { get; set; }
+        public int b { get; set; }
+        public String operacao { get; set; }
 
-        public int somar(int a, int b)
+        public int somar()
         {
             return valor = a + b;
 
         }
 
-        public int subtrair(int a, int b)
+        public int subtrair()
         {
             return valor = a - b;
         }
@@ -24,6 +27,11 @@ namespace Aula2
         public int ultimoResultado()
         {
             return valor;
+        }
+
+        public string ultimaOperacao()
+        {
+            return valor == somar() ? "Soma" : "Subtracao";
         }
 
     }
