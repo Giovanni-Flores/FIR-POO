7ac07a4c748eaf3cee808616935c8daf25083a52
Giovanni Flores <66764596+Giovanni-Flores@users.noreply.github.com>
2023-05-06 16:04:20 -0300

    Add files via upload

---
+++ Aula3/Aula2/Aula2/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    class Calculadora
+    {
+        public int valor { get; set; }
+        public int a { get; set; }
+        public int b { get; set; }
+        public String operacao { get; set; }
+
+        public int somar()
+        {
+            return valor = a + b;
+
+        }
+
+        public int subtrair()
+        {
+            return valor = a - b;
+        }
+
+        public int ultimoResultado()
+        {
+            return valor;
+        }
+
+        public string ultimaOperacao()
+        {
+            return valor == somar() ? "Soma" : "Subtracao";
+        }
+
+    }
+}
---
+++ Aula3/Aula2/Aula2/Computador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    class Computador
+    {
+           public String nome { get; set; }
+           public int clock { get; set; }
+
+            public String mostraNomeComputador()
+            {
+                return nome;
+            }
+
+            public int mostraClock()
+            {
+                return clock;
+            }
+    }
+}
